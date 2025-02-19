f38dc938b1edb426d7e7f9a7b8eedb01893b89d5
yoro1836 <ijaeyong46@outlook.kr>
2025-02-19 13:16:21 +0800

    fixed AI's poop

--- app/src/main/java/cn/xylin/skiprewardad/Start.java
+++ app/src/main/java/cn/xylin/skiprewardad/Start.java
@@ -23,38 +23,38 @@ import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class Start implements IXposedHookLoadPackage {
-    private int hash;
-    @Override
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam load) {
-        if (!load.isFirstApplication || !load.appInfo.processName.equals(load.processName)) {
-            return;
-        }
-        XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                if (hash == hashCode()) {
-                    return;
-                }
-                hash = hashCode();
-                startHook((Context) param.args[0]);
-            }
-        });
-    }
-    
-    private synchronized void startHook(Context baseContext) {
-        new GdtAdHook1(baseContext);
-        new GdtAdHook2(baseContext);
-        new FusionAdHook(baseContext);
-        new TTAdHook(baseContext);
-        new BaiduAdHook(baseContext);
-        new SigmobAdHook(baseContext);
-        new MintegralAdHook(baseContext);
-        new UnityAdHook1(baseContext);
-        new UnityAdHook2(baseContext);
-        new KsAdHook(baseContext);
-        new VungleAdHook(baseContext);
-        new GoogleAdHook1(baseContext);
-        new ApplovinAdHook(baseContext);
-        new IronSourceAdHook(baseContext); // 여기에 새로운 Hook 추가
-    }
+    private int hash;
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam load) {
+        if (!load.isFirstApplication || !load.appInfo.processName.equals(load.processName)) {
+            return;
+        }
+        XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                if (hash == hashCode()) {
+                    return;
+                }
+                hash = hashCode();
+                startHook((Context) param.args[0]);
+            }
+        });
+    }
+    
+    private synchronized void startHook(Context baseContext) {
+        new GdtAdHook1(baseContext);
+        new GdtAdHook2(baseContext);
+        new FusionAdHook(baseContext);
+        new TTAdHook(baseContext);
+        new BaiduAdHook(baseContext);
+        new SigmobAdHook(baseContext);
+        new MintegralAdHook(baseContext);
+        new UnityAdHook1(baseContext);
+        new UnityAdHook2(baseContext);
+        new KsAdHook(baseContext);
+        new VungleAdHook(baseContext);
+        new GoogleAdHook1(baseContext);
+        new ApplovinAdHook(baseContext);
+        new IronSourceAdHook(baseContext); // 여기에 새로운 Hook 추가
+    }
 }
